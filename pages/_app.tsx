b519309b1b7e274b64091b5ed28f4da82359d5b5
Diego-Paris <diego.paris@upr.edu>
2023-03-29 00:23:55 -0400

    Added react query info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import  { useState } from 'react';
 
 function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+  
   return (
     <>
       <SessionProvider session={pageProps.session}>
-        <ChakraProvider>
-          <Navbar />
-          <Component {...pageProps} />
-        </ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+          <ChakraProvider>
+            <Navbar />
+            <Component {...pageProps} />
+            <ReactQueryDevtools />
+          </ChakraProvider>
+        </QueryClientProvider>
       </SessionProvider>
     </>
   );
