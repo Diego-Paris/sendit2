0dfad6b9cb4ebd7643d27acd0ee15354ad2a0eda
Diego-Paris <diego.paris@upr.edu>
2023-04-02 20:59:12 -0400

    Updated page wrapper to receive styles

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,6 +1,6 @@
 import { Flex } from "@chakra-ui/react";
 
-const PageWrapper = ({ children }) => {
+const PageWrapper = ({ children, styles = {} }) => {
   return (
     <Flex
       bg="gray.100"
@@ -10,6 +10,7 @@ const PageWrapper = ({ children }) => {
       h="auto"
       minH={`calc(100vh - 73px)`}
       flexGrow={1}
+      {...styles}
     >
       {children}
     </Flex>
