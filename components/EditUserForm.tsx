e8991f9e5910f29dfb6b04f6283e508fa11cf0ad
Diego-Paris <diego.paris@upr.edu>
2023-04-04 18:17:14 -0400

    eh

--- components/EditUserForm.tsx
+++ components/EditUserForm.tsx
@@ -19,9 +19,9 @@ import {
 import { SmallCloseIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useFormik } from "formik";
 import { useRouter } from "next/router";
-
+import userValidationSchema from "@/validations/UserSchema";
+import { Formik, Form, Field } from "formik";
 const UserProfileEdit = ({ user, session, setSwalProps }) => {
   const queryClient = useQueryClient();
   const mutation = useMutation(updateUserData);
@@ -29,26 +29,20 @@ const UserProfileEdit = ({ user, session, setSwalProps }) => {
   const toast = useToast();
   const [newUsername, setNewUsername] = useState(user.username);
   const [usernameExists, setUsernameExists] = useState(true);
-  const [isValid, setIsValid] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
-  const formik = useFormik({
-    initialValues: {
-      name: user?.name || "",
-      username: user?.username || "",
-      email: user?.email || "",
-    },
-    onSubmit: (values) => {
-      mutation.mutate({
-        ...values,
-        email: user.email, // Pass the current email as it shouldn't be updated
-      });
-    },
+  const [initialValues, setInitialValues] = useState({
+    name: user?.name || "",
+    username: user?.username || "",
+    email: user?.email || "",
   });
 
   useEffect(() => {
     if (user) {
-      console.log(user);
+      // setInitialValues({
+      //   name: user?.name || "",
+      //   username: user?.username || "",
+      //   email: user?.email || "",
+      // });
     }
     if (session) {
       console.log(session);
@@ -141,255 +135,123 @@ const UserProfileEdit = ({ user, session, setSwalProps }) => {
         p={6}
         // my={12}
       >
-        {/* <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-          User Profile Edit
-        </Heading> */}
-        <FormControl id="userName">
-          <FormLabel>User Icon</FormLabel>
-          <Stack direction={["column", "row"]} spacing={6}>
-            <Center>
-              <Avatar size="xl" src={user?.image}>
-                <AvatarBadge
-                  as={IconButton}
-                  size="sm"
-                  rounded="full"
-                  top="-10px"
-                  colorScheme="red"
-                  aria-label="remove Image"
-                  isDisabled
-                  icon={<SmallCloseIcon />}
-                />
-              </Avatar>
-            </Center>
-            <Center w="full">
-              <Button w="full" isDisabled>
-                Change Icon
-              </Button>
-            </Center>
-          </Stack>
-        </FormControl>
-        <form onSubmit={formik.handleSubmit}>
-          <FormControl id="name" isRequired marginBottom={3}>
-            <FormLabel>Name</FormLabel>
-            <Input
-              name="name"
-              placeholder="Your Name"
-              _placeholder={{ color: "gray.500" }}
-              type="text"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-            />
-          </FormControl>
-          <FormControl
-            id="username"
-            isInvalid={usernameExists && formik.dirty}
-            isRequired
-            marginBottom={3}
-          >
-            <FormLabel>Username</FormLabel>
-            <Input
-              name="username"
-              placeholder="Username"
-              _placeholder={{ color: "gray.500" }}
-              type="text"
-              value={formik.values.username}
-              onChange={(event) => {
-                formik.handleChange(event);
-                handleChange(event);
-              }}
-            />
-            {!isLoading && usernameExists &&  (
-              <FormErrorMessage>Username is taken</FormErrorMessage>
-            )}
-            {isLoading && (
-              <FormHelperText textAlign="left" color="grey.400">
-                Checking availability
-              </FormHelperText>
-            )}
-            {(!usernameExists || newUsername === user.username) &&
-              !isLoading && (
-                <FormHelperText textAlign="left" color="green.400">
-                  Username is available
-                </FormHelperText>
-              )}
-          </FormControl>
-          <FormControl id="email" marginBottom={10}>
-            <FormLabel>Email Address</FormLabel>
-            <Input
-              name="email"
-              placeholder="Email Address"
-              _placeholder={{ color: "gray.500" }}
-              type="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              isDisabled
-              bg="gray.200"
-            />
-            <FormHelperText color="gray.400">
-              We&apos;ll never share your email with anyone else.
-            </FormHelperText>
-          </FormControl>
-          <HStack justify="flex-end">
-            <Button
-              type="submit"
-              colorScheme="teal"
-              isLoading={mutation.isLoading}
-              w="full"
-              marginBottom={0}
-              isDisabled={!formik.dirty || !formik.isValid || usernameExists}
-            >
-              Save changes
-            </Button>
-          </HStack>
-        </form>
-        <Button
-          colorScheme="purple"
-          w="full"
-          onClick={() => {
-            setSwalProps({ show: false });
+        <Formik
+          initialValues={initialValues}
+          validationSchema={userValidationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            // setTimeout(() => {
+            //   alert(JSON.stringify(values, null, 2));
+            //   setSubmitting(false);
+            // }, 400);
+            mutation.mutate({
+              ...values,
+              email: user.email, // Pass the current email as it shouldn't be updated
+            });
+            setSubmitting(false);
           }}
         >
-          Return to profile
-        </Button>
+          {({ isSubmitting, isValid, dirty, errors, touched, values }) => (
+            <Form>
+              <Field name="name">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={!!(errors.name && touched.name)}
+                    isRequired
+                  >
+                    <FormLabel htmlFor="name">Name</FormLabel>
+                    <Input
+                      {...field}
+                      id="name"
+                      placeholder="Name"
+                      value={values.name}
+                    />
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="username">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={!!(errors.username && touched.username)}
+                    isRequired
+                  >
+                    <FormLabel htmlFor="username">Username</FormLabel>
+                    <Input
+                      {...field}
+                      id="username"
+                      placeholder="Username"
+                      value={values.username}
+                      onChange={(e) => {
+                        form.setFieldValue("username", e.target.value);
+                        handleChange(e);
+                      }}
+                    />
+                    {!isLoading &&
+                      usernameExists &&
+                      newUsername !== user.username && (
+                        <FormHelperText textAlign="left" color="red.400">
+                          Username is not available
+                        </FormHelperText>
+                      )}
+                    {isLoading && (
+                      <FormHelperText textAlign="left" color="grey.400">
+                        Checking availability
+                      </FormHelperText>
+                    )}
+                    {(!usernameExists || newUsername === user.username) &&
+                      !isLoading && (
+                        <FormHelperText textAlign="left" color="green.400">
+                          Username is available
+                        </FormHelperText>
+                      )}
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="email">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={!!(errors.email && touched.email)}
+                    isRequired
+                  >
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      placeholder="Email"
+                      value={values.email}
+                    />
+                  </FormControl>
+                )}
+              </Field>
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={isSubmitting}
+                type="submit"
+                isDisabled={
+                  !dirty ||
+                  !isValid ||
+                  usernameExists ||
+                  isLoading ||
+                  (initialValues.name === values.name &&
+                    initialValues.username === values.username &&
+                    initialValues.email === values.email)
+                }
+              >
+                Submit
+              </Button>
+              {Object.keys(errors).length > 0 && (
+                <div>
+                  {Object.keys(errors).map((fieldName) => (
+                    <div key={fieldName}>{errors[fieldName]}</div>
+                  ))}
+                </div>
+              )}
+            </Form>
+          )}
+        </Formik>
       </Stack>
     </Flex>
   );
 };
 
 export default UserProfileEdit;
-
-// import {
-//   Button,
-//   Flex,
-//   FormControl,
-//   FormLabel,
-//   Heading,
-//   Input,
-//   Stack,
-//   useColorModeValue,
-//   HStack,
-//   Avatar,
-//   AvatarBadge,
-//   IconButton,
-//   Center,
-// } from '@chakra-ui/react';
-// import { SmallCloseIcon } from '@chakra-ui/icons';
-// import { useEffect } from 'react'
-
-// export default function UserProfileEdit({ user, session }): JSX.Element {
-//   useEffect(() => {
-//     // Your code here
-//     if (user) {
-//       console.log(user)
-//     }
-//     if (session) {
-//       console.log(session)
-//     }
-//   }, [user, session])
-
-//   return (
-//     <Flex
-//       // minH={'100vh'}
-//       align={'center'}
-//       justify={'center'}
-//       bg={useColorModeValue('white', 'gray.800')}>
-//       <Stack
-//         spacing={4}
-//         w={'full'}
-//         maxW={'md'}
-//         bg={useColorModeValue('white', 'gray.700')}
-//         // rounded={'xl'}
-//         // boxShadow={'lg'}
-//         p={6}
-//         // my={12}
-//         >
-//         {/* <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-//           User Profile Edit
-//         </Heading> */}
-//         <FormControl id="userName">
-//           <FormLabel>User Icon</FormLabel>
-//           <Stack direction={['column', 'row']} spacing={6}>
-//             <Center>
-//               <Avatar size="xl" src="https://bit.ly/sage-adebayo">
-//                 <AvatarBadge
-//                   as={IconButton}
-//                   size="sm"
-//                   rounded="full"
-//                   top="-10px"
-//                   colorScheme="red"
-//                   aria-label="remove Image"
-//                   icon={<SmallCloseIcon />}
-//                 />
-//               </Avatar>
-//             </Center>
-//             <Center w="full">
-//               <Button w="full">Change Icon</Button>
-//             </Center>
-//           </Stack>
-//         </FormControl>
-//         <FormControl id="name" isRequired>
-//           <FormLabel>Name</FormLabel>
-//           <Input
-//           defaultValue={`${user.name}`}
-//             placeholder="Your Name"
-//             _placeholder={{ color: 'gray.500' }}
-//             type="text"
-//           />
-//         </FormControl>
-//         <FormControl id="userName" isRequired>
-//           <FormLabel>Username</FormLabel>
-//           <Input
-//           defaultValue={`${user.username}`}
-//             placeholder="Your Name"
-//             _placeholder={{ color: 'gray.500' }}
-//             type="text"
-//           />
-//         </FormControl>
-//         <FormControl id="email" isRequired>
-//           <FormLabel>Email address</FormLabel>
-//           <Input
-//             placeholder={`${user.email}`}
-//             _placeholder={{ color: 'gray.500' }}
-//             type="email"
-//             // isReadOnly
-//             // variant='filled'
-//             // isDisabled
-//             // color="red"
-//             focusBorderColor="white"
-//             bg="gray.200"
-//             pointerEvents="none"
-//             defaultValue={`${user.email}`}
-//           />
-//         </FormControl>
-//         {/* <FormControl id="password" isRequired>
-//           <FormLabel>Password</FormLabel>
-//           <Input
-//             placeholder="password"
-//             _placeholder={{ color: 'gray.500' }}
-//             type="password"
-//           />
-//         </FormControl> */}
-//         <Stack spacing={6} direction={['column', 'row']}>
-//           <Button
-//             bg={'red.400'}
-//             color={'white'}
-//             w="full"
-//             _hover={{
-//               bg: 'red.500',
-//             }}>
-//             Cancel
-//           </Button>
-//           <Button
-//             bg={'blue.400'}
-//             color={'white'}
-//             w="full"
-//             _hover={{
-//               bg: 'blue.500',
-//             }}>
-//             Submit
-//           </Button>
-//         </Stack>
-//       </Stack>
-//     </Flex>
-//   );
-// }
